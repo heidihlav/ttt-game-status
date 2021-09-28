14747ac8e26c1a554e5c7c25427c95b572918e60
Heidi Hlavinka <hchlav@gmail.com>
2021-09-27 20:35:02 -0500

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,47 @@
+require 'pry'
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+
+]
+
+def won?(board)
+  WIN_COMBINATIONS.find { 
+    |array| board[array[0]] == board[array[1]] && 
+    board[array[1]] == board[array[2]] &&
+    position_taken?(board, array[1])
+  }
+end
+
+
+def full?(board)
+  board.none? {|token|
+    token ==  " "
+  }
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  draw?(board) || won?(board)
+end
+
+def winner(board)
+
+end
+
